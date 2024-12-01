4616f6fdbc5ca89feed4a1691fac2c95bcdc6ed9
thisispramod <itspramod19@gmail.com>
2024-12-01 16:52:13 +0530

    working with sub category for more check it

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\SubCategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
@@ -21,5 +22,8 @@ Route::resource('slider', SliderController::class);
 
 /** Category Routes */
 Route::put('change-status', [CategoryController::class,'changeStatus'])->name('category.change-status');
-
 Route::resource('category', CategoryController::class);
+
+/** Sub Category Route */
+Route::put('subcategory/change-status', [SubCategoryController::class, 'changeStatus'])->name('sub-category.change-status');
+Route::resource('sub-category', SubCategoryController::class);
